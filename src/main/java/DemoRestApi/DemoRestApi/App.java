4bc226fa4f0f3fea4ec563c93697cbd322da1f3f
Sumanth <uniquesumanth143@gmail.com>
2024-10-15 15:48:22 +0530

    basics rest assured practise codes

---
+++ src/main/java/DemoRestApi/DemoRestApi/App.java
@@ -0,0 +1,13 @@
+package DemoRestApi.DemoRestApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
